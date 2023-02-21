cd740f640b55c7d12b287a31749528f4cb961a48
Muhammad Wafa <muhammadwafa105@gmail.com>
2023-02-21 15:11:51 +0700

    try create and insert to database

--- app/src/main/java/com/mikirinkode/myfirstrealm/MainActivity.kt
+++ app/src/main/java/com/mikirinkode/myfirstrealm/MainActivity.kt
@@ -2,10 +2,61 @@ package com.mikirinkode.myfirstrealm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mikirinkode.myfirstrealm.databinding.ActivityMainBinding
+import io.realm.kotlin.Realm
+import io.realm.kotlin.ext.query
+import io.realm.kotlin.query.RealmResults
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val database: Realm by lazy {
+        Database.getDatabase()
+    }
+
+    private val userAdapter: UserAdapter by lazy {
+        UserAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        observeData()
+
+        binding.apply {
+            rvUser.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvUser.adapter = userAdapter
+
+            btnSubmit.setOnClickListener {
+                submit()
+            }
+        }
+    }
+
+    private fun observeData() {
+        val users: RealmResults<User> = database.query<User>().find()
+        userAdapter.setList(users)
+    }
+
+    private fun submit() {
+        Toast.makeText(this, "SUBMIT CALLED", Toast.LENGTH_SHORT).show()
+        binding.apply {
+            val inputName = edtName.text.toString()
+            val inputEmail = edtEmail.text.toString()
+
+            database.writeBlocking {
+                copyToRealm(User().apply {
+                    name = inputName
+                    email = inputEmail
+                })
+            }
+        }
+        Toast.makeText(this, "DONE", Toast.LENGTH_SHORT).show()
     }
 }
